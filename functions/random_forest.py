61782db8f188c26ffafab787254c3c7e12f2a018
NefariousNiru <nirupomboseroy99@gmail.com>
2024-11-24 05:06:39 -0500

    optimize hyperparameter for random forest

--- functions/random_forest.py
+++ functions/random_forest.py
@@ -7,29 +7,7 @@ def run_rf(train_feature_file, train_label_file, test_feature_file, test_label_f
     train_features, train_labels, test_features, test_labels, classes = (
         data_util.feature_pre_processing(train_feature_file, train_label_file, test_feature_file, test_label_file, smote=True))
 
-    param_dist = {
-        'n_estimators': randint(100, 300),
-        'max_depth': [10, 20],
-        'min_samples_split': randint(2, 20),
-        'min_samples_leaf': randint(1, 5),
-    }
+    rf = RandomForestClassifier(n_estimators=300, max_depth=20, min_samples_leaf=3, min_samples_split=8, random_state=42, n_jobs=4)
+    rf.fit(train_features, train_labels)
 
-    rf_model = RandomForestClassifier(random_state=42, n_jobs=-1)
-    # Perform Randomized Search
-    random_search = RandomizedSearchCV(
-        estimator=rf_model,
-        param_distributions=param_dist,
-        n_iter=20,
-        scoring='f1_weighted',
-        cv=2,
-        verbose=2,
-        n_jobs=-1,
-        random_state=42
-    )
-
-    random_search.fit(train_features, train_labels)
-
-    print("Best Params")
-    print(random_search.best_params_)
-
-    data_util.f_statistics(random_search.best_estimator_, test_features, test_labels, classes)
+    data_util.f_statistics(rf, test_features, test_labels, classes)
